70c7200a8957e220fe3c5ce312ec547631154c81
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-05 15:55:48 -0500

    Add controller

---
+++ Cars/Cars.API/Controllers/CarsController.cs
@@ -0,0 +1,92 @@
+﻿using Cars.API.Data;
+using Cars.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cars.API.Controllers
+{
+    [ApiController]
+    [Route("/api/cars")]
+    public class CarsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CarsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Cars.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var car = await _context.Cars
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (car is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Car car)
+        {
+            _context.Add(car);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(car);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                return BadRequest(dbUpdateException.InnerException!.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Car car)
+        {
+            _context.Update(car);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(car);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                return BadRequest(dbUpdateException.InnerException!.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var car = await _context.Cars
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(car);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
+
---
+++ Cars/Cars.API/Data/SeedDb.cs
@@ -0,0 +1,34 @@
+﻿using Cars.Shared.Entities;
+
+namespace Cars.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCarsAsync();
+        }
+
+        private async Task CheckCarsAsync()
+        {
+            if (!_context.Cars.Any())
+            {
+                _context.Cars.Add(new Car { Make = "Audi", Model = "R8", Year = 2018, Doors = 2, Color = "Red", Price = 79995 });
+                _context.Cars.Add(new Car { Make = "Tesla", Model = "3", Year = 2018, Doors = 4, Color = "Black", Price = 54995 });
+                _context.Cars.Add(new Car { Make = "Porsche", Model = " 911 991", Year = 2020, Doors = 2, Color = "White", Price = 155000 });
+                _context.Cars.Add(new Car { Make = "Mercedes-Benz", Model = "GLE 63S", Year = 2021, Doors = 5, Color = "Blue", Price = 83995 });
+                _context.Cars.Add(new Car { Make = "BMW", Model = "X6 M", Year = 2020, Doors = 5, Color = "Silver", Price = 62995 });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
