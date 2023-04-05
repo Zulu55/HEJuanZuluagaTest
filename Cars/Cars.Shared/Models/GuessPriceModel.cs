946c07020224f6b0a72fb6f2ba73bdf66a204f10
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-04-05 16:50:46 -0500

    Guess price

---
+++ Cars/Cars.Shared/Models/GuessPriceModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Cars.Shared.Models
+{
+    public class GuessPriceModel
+    {
+        public decimal Price { get; set; }
+    }
+}
---
+++ Cars/Cars.WEB/Repositories/HttpResponseWrapper .cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+namespace Cars.WEB.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        public bool Error { get; set; }
+
+        public T? Response { get; set; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public async Task<string?> GetErrorMessageAsync()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var codigoEstatus = HttpResponseMessage.StatusCode;
+            if (codigoEstatus == HttpStatusCode.NotFound)
+            {
+                return "Resource not found";
+            }
+            else if (codigoEstatus == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if (codigoEstatus == HttpStatusCode.Unauthorized)
+            {
+                return "You have to log in to do this operation";
+            }
+            else if (codigoEstatus == HttpStatusCode.Forbidden)
+            {
+                return "You do not have permissions to do this operation";
+            }
+
+            return "An unexpected error has occurred";
+        }
+    }
+}
